1e9a4df2d9f88285f1c61df9fa654d344903be34
yugantx@gmail.com <yugantx@gmail.com>
2023-03-19 20:20:21 +0530

    need deployment and update readme

---
+++ components/jobdetails/about/about.style.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS, FONT, SIZES } from "../../../constants";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: SIZES.large,
+    backgroundColor: "#3d3d3d",
+    borderRadius: SIZES.medium,
+    padding: SIZES.medium,
+  },
+  headText: {
+    fontSize: SIZES.large,
+    color: COLORS.white,
+    fontFamily: FONT.bold,
+  },
+  contentBox: {
+    marginVertical: SIZES.small,
+  },
+  contextText: {
+    fontSize: SIZES.medium - 2,
+    color: COLORS.gray,
+    fontFamily: FONT.regular,
+    marginVertical: SIZES.small / 1.25,
+  },
+});
+
+export default styles;
